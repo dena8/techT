7663d6e9a5c1780c05c41ed51d36572b4342bc72
dena8 <Perindopril88@>
2022-05-28 09:56:09 +0300

    initial commit

---
+++ src/main/java/com/example/techtopic/TechTopicApplication.java
@@ -0,0 +1,13 @@
+package com.example.techtopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechTopicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/techtopic/TechTopicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techtopic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTopicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
